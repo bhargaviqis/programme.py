d538c2e6d5f27b4f3cc64740589f4cf9615273d0
bhargaviqis <42573061+bhargaviqis@users.noreply.github.com>
2019-02-20 11:17:55 +0530

    Update prime number between two numbers.py

--- prime number between two numbers.py
+++ prime number between two numbers.py
@@ -9,6 +9,6 @@ for num in range(x+1,y):
         l.append(num)
 for p in range(0,len(l)):
   if(p==len(l)-1):
-    print(l[p],end=(""))
+    print(l[p],end="")
   else:
-    print(l[p],end=(" "))
+    print(l[p],end=" ")
